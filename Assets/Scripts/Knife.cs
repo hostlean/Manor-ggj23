bf978470cb4fd3f93662871872d41202c642273f
hostlean <60171210+hostlean@users.noreply.github.com>
2023-02-04 00:35:37 +0300

    Edited: Knfe

--- Assets/Scripts/Knife.cs
+++ Assets/Scripts/Knife.cs
@@ -9,22 +9,7 @@ namespace Manor
     public class Knife : MonoBehaviour
     {
         [SerializeField] private GameEventListener eventListener;
-        [SerializeField] private FloatVariable healthVariable;
-
-
-        private void Awake()
-        {
-            healthVariable.OnValueChanged += CheckHealthAndKill;
-        }
-
-        private void CheckHealthAndKill()
-        {
-            if (healthVariable.Value <= 0)
-            {
-                Debug.Log("DIEEE!!!");
-            }
-        }
-
+        
         public void Cut()
         {
             Debug.Log("Cut!!!!");
